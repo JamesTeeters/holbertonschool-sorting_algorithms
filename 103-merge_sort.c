b6d8d3e667247669633ac03dee608ae411b0cddc
JamesTeeters <4925@holbertonstudents.com>
2023-01-16 13:45:33 -0600

    fixed compile error

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -1,5 +1,6 @@
 #include "sort.h"
 
+
 /**
  * merge_sort - sorts an array of integers in ascending order
  * using the top-down merge sort algorithm
@@ -20,21 +21,22 @@ void merge_sort(int *array, size_t size)
  * merge_sort_recursion - Recursively calls the merge sort function
  * until all arrays are sorted
  * @array: pointer to the array
- * @l: left-most (first) index of the array
- * @r: right-most (last) index of the array
+ * @left: left-most (first) index of the array
+ * @right: right-most (last) index of the array
  * Return: void
  */
-void merge_sort_recursion(int *array, int l, int r)
+void merge_sort_recursion(int *array, int left, int right)
 {
-    if (r <= l)
+    int mid = ((right - left - 1) / 2) + left;
+
+    if (right <= left)
         return;
     else
     {
-        int m = l + (r - l) / 2;
-        merge_sort_recursion(array, l, m);
-        merge_sort_recursion(array, m + 1, r);
-        merge_sorted_arrays(array, l, m, r);
-        print_array_status("Done", &array[l], (r - l + 1));
+        merge_sort_recursion(array, left, mid);
+        merge_sort_recursion(array, mid + 1, right);
+        merge_sorted_arrays(array, left, mid, right);
+        print_array_status("Done", &array[left], (right - left + 1));
     }
 }
 
@@ -42,42 +44,54 @@ void merge_sort_recursion(int *array, int l, int r)
  * merge_sorted_arrays - this is the function that applies the merge
  * sort algorithm on each iteration
  * @array: The address of the array being sorted
- * @l: left-most (first) index of the array
- * @m: the break point of the array being sorted
- * @r: right-most (last) index of the array
+ * @left: left-most (first) index of the array
+ * @mid: the break point of the array being sorted
+ * @right: right-most (last) index of the array
  * Return: void
  */
-void merge_sorted_arrays(int *array, int l, int m, int r)
+void merge_sorted_arrays(int *array, int left, int mid, int right)
 {
-    int left_length = m - l + 1;
-    int right_length = r - m;
-
-    int tmp_left[left_length];
-    int tmp_right[right_length];
+    int left_length = mid - left + 1;
+    int right_length = right - mid;
+    int temp_left[4096], temp_right[4096];
     int i, j, k;
 
     printf("Merging...\n");
-    for (int i = 0; i < left_length; i++)
-        tmp_left[i] = array[l + i];
-    print_array_status("left", &array[l], left_length);
-
-    for(int i = 0; i < right_length; i++)
-        tmp_right[i] = array[m + 1 + i];
-    print_array_status("right", &array[m + 1], right_length);
-
-    for (i = 0, j = 0, k = l; k <= r; k++)
+    for (i = 0; i < left_length; i++)
+        temp_left[i] = array[left + i];
+    print_array_status("left", &array[left], left_length);
+    for (i = 0; i < right_length; i++)
+        temp_right[i] = array[mid + 1 + i];
+    print_array_status("right", &array[mid + 1], right_length);
+    i = 0, j = 0, k = left;
+    while (i < left_length && j < right_length)
     {
-        if ((i < left_length && j >= right_length) || tmp_left[i] <= tmp_right[j])
+        if (temp_left[i] <= temp_right[j])
         {
-            array[k] = tmp_left[i];
+            array[k] = temp_left[i];
             i++;
         }
         else
         {
-            array[k] = tmp_right[j];
+            array[k] = temp_right[j];
             j++;
         }
-    }  
+        k++;
+    }
+
+    while (i < left_length)
+    {
+        array[k] = temp_left[i];
+        i++;
+        k++;
+    }
+
+    while (j < right_length)
+    {
+        array[k] = temp_right[j];
+        j++;
+        k++;
+    }
 }
 
 /**
